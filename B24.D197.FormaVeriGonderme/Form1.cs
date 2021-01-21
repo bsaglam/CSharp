a4db08b19a03c864fe4c289af65ebc1cd09383bd
bsaglam <burcu.saglam001@gmail.com>
2021-01-21 15:38:56 +0300

    Form yapıcı metodunu parametre ile açma

---
+++ B24.D197.FormaVeriGonderme/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B24.D197.FormaVeriGonderme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MerhabaForm form = new MerhabaForm(txtBoxVeri.Text);
+            form.Show();
+        }
+    }
+}
---
+++ B24.D197.FormaVeriGonderme/MerhabaForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B24.D197.FormaVeriGonderme
+{
+    public partial class MerhabaForm : Form
+    {
+        public MerhabaForm(string data)
+        {
+            InitializeComponent();
+            label1.Text = data;
+
+        }
+
+        private void MerhabaForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
