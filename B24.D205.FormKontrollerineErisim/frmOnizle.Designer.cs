619409767e0d9134a40bc38ef82e1a72691e4392
bsaglam <burcu.saglam001@gmail.com>
2021-01-23 06:48:29 +0300

    aık olan formdan bir önceki forma geçiş

--- B24.D205.FormKontrollerineErisim/frmOnizle.Designer.cs
+++ B24.D205.FormKontrollerineErisim/frmOnizle.Designer.cs
@@ -96,6 +96,7 @@
             this.btnKaydet.TabIndex = 9;
             this.btnKaydet.Text = "Kaydet";
             this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
             // 
             // frmOnizle
             // 
--- B24.D205.FormKontrollerineErisim/frmOnizle.cs
+++ B24.D205.FormKontrollerineErisim/frmOnizle.cs
@@ -16,5 +16,26 @@ namespace B24.D205.FormKontrollerineErisim
         {
             InitializeComponent();
         }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            //Bu kaydet butonuna bastıgımda, açık olan formlar arasından bir önceki formu bulup ona geçiş yapabilmeli
+            // bunun için Application.Openforms 'u kullanabiliriz.
+
+            foreach (Form item in Application.OpenForms) //Form ları sırasıyla geziyorum
+            {
+                if (item.Name == "Form1") //Geçş yapacağım formu buluyorum
+                {
+                    foreach (Control itemControl in item.Controls) //geçiş yapcağım formun controllerine erişip, null yapıyorum
+                    {
+                        if (itemControl is TextBox)
+                        {
+                            ((TextBox)itemControl).Text = String.Empty;
+                        }
+                    }
+                }
+            }
+            this.Close();//açık olan formu kapatıyorum.
+        }
     }
 }
