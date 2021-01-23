59e8586f617b21f16ac79b5f8d85d261277f34b0
bsaglam <burcu.saglam001@gmail.com>
2021-01-23 07:25:57 +0300

    runtime de dinamik butonlar, FlowLayoutPnel içinde oluşturuldu.

---
+++ B24.D208.DinamikFormElemanlari/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace B24.D208.DinamikFormElemanlari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(496, 2);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(105, 66);
+            this.btnGoster.TabIndex = 1;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(478, 228);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 242);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnGoster);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ B24.D208.DinamikFormElemanlari/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B24.D208.DinamikFormElemanlari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Button btn = new Button();
+                //btn.Location = new System.Drawing.Point(12, 12);
+                btn.Name = "button" + i;
+                btn.Size = new System.Drawing.Size(35, 35);
+                btn.TabIndex = 0;
+                btn.Text = i.ToString();
+                btn.UseVisualStyleBackColor = true;
+                flowLayoutPanel1.Controls.Add(btn);
+
+            }
+        }
+    }
+}
