c91b6df4e20418e143c37d41f69b90c6a9de12fa
bsaglam <burcu.saglam001@gmail.com>
2021-01-24 14:37:07 +0300

    GridView'e image eklendi.

---
+++ B24.D229.GridViewImageGosterme/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B24.D229.GridViewImageGosterme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Urun> musteri = new List<Urun>() {
+                new Urun{Id=1,ResimYol=@"C:\Users\GMYLOJ59\Desktop\Keys-icon.png",UrunTanim="aaaabbbcc"},
+                new Urun{Id=2,ResimYol=@"C:\Users\GMYLOJ59\Desktop\Keys-icon.png",UrunTanim="bbbbbbbbb"},
+                new Urun{Id=3,ResimYol=@"C:\Users\GMYLOJ59\Desktop\Keys-icon.png",UrunTanim="ccccccccc"},
+
+            };
+            dataGridView1.DataSource = musteri;
+        }
+    }
+}
---
+++ B24.D229.GridViewImageGosterme/Urun.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B24.D229.GridViewImageGosterme
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public String UrunTanim { get; set; }
+        public string ResimYol { get; set; }
+        public Image Resim
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ResimYol))
+                {
+                    return Image.FromFile(ResimYol);
+                }
+                else
+                {
+                    return Image.FromFile(@"C:\Users\GMYLOJ59\Desktop\Keys-icon.png");
+                }
+
+            }
+        }
+    }
+}
